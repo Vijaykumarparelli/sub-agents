ef9fce4b392d25d0a5885dad59d8333fc0e984c5
vparell <vijaykumar_parelli@gap.com>
2026-07-13 18:48:45 +0530

    init

--- src/app/api/properties/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { properties } from "@/lib/properties";
-
-export async function GET(): Promise<NextResponse> {
-  return NextResponse.json(properties);
-}
